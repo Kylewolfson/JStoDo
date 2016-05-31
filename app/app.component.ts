6bc46d735f6501d6ee7c8f824112abd38ba28752
Andrew Russo and Kyle Wolfson <kyle@>
2016-05-31 15:12:22 -0700

    Updated code to allow it to run task list and allow you to click and select specific lines of the list.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)"
+  [class.selected]="currentTask === selectedTask">
+  {{ currentTask.description }}
+  </h3>
+  `
+})
+
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">{{ task.description }}</h3>
+      <task-list [taskList]="tasks" (onTaskSelect)="taskWasSelected($event)"></task-list>
     </div>
   `
 })
@@ -30,44 +57,3 @@ export class Task {
 
   }
 }
-
-// @Component({
-//   selector: 'task-list',
-//   inputs: ['taskList'],
-//   template: `
-//   <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-//     {{ currentTask.description }}
-//   </h3>
-//   `
-// })
-
-export class TaskListComponent {
-  public taskList: Task[];
-  taskClicked(clickedTask: Task): void {
-    console.log(clickedTask);
-  }
-}
-
-// export class AppComponent {
-//   public tasks: Task[];
-//   constructor(){
-//     this.tasks = [
-//       new Task("Create To-Do List app", 0),
-//       new Task("Learn Kung Fu", 1),
-//       new Task("Rewatch all of the Lord of the Rings movies.", 2),
-//       new Task("Do the laundry", 3)
-//     ];
-//   }
-//   taskWasSelected(clickedTask: Task): void {
-//     console.log(clickedTask);
-//   }
-// }
-//
-//
-//
-// export class Task {
-//   public done: boolean = false;
-//   constructor(public description: string, public id: number){
-//
-//   }
-// }
